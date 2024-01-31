31cbf3ce5837c8e1ccbb8ed150c44147665e1226
Camilo Rodelo <camilo_jrl@hotmail.com>
2024-01-31 00:25:48 -0500

    Almost finishing

--- 02-tic-tac-toe/app/page.tsx
+++ 02-tic-tac-toe/app/page.tsx
@@ -1,113 +1,142 @@
-import Image from "next/image";
+"use client";
+import { openSans } from "./ui/fonts";
+import { useState } from "react";
+
+type SquareProps = {
+  children?: string;
+  index?: number;
+  isSelected?: boolean;
+  hasBorder?: boolean;
+  updateBoard?: (index: number) => void;
+};
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({
+  children,
+  index,
+  isSelected,
+  hasBorder,
+  updateBoard,
+}: SquareProps) => {
+  const className = `${isSelected ? "bg-slate-500" : ""} ${
+    hasBorder ? "border-[2px] border-white" : "text-xl w-[80px] h-[80px]"
+  }`;
+
+  const handleClick = () => {
+    updateBoard && updateBoard(index as number);
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`${className} w-[100px] h-[100px] grid place-items-center cursor-pointer text-5xl rounded-lg`}
+    >
+      {children}
+    </div>
+  );
+};
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 export default function Home() {
+  const [board, setBoard] = useState<string[]>(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState<string | null | false>(null);
+
+  const checkWinner = (boardToCheck: string[]) => {
+    for (const combo of WINNER_COMBOS) {
+      //recuperamos las posiciones
+      const [a, b, c] = combo;
+      if (
+        //*verificamos si en la posicion a existe algo
+        boardToCheck[a] &&
+        //*verificamos si lo que hay en a === lo que hay en b
+        boardToCheck[a] === boardToCheck[b] &&
+        //*verificamos si lo que hay en b === lo que hay en c
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        //*devolvemos al ganador que será el que esté en la posición a
+        return boardToCheck[a];
+      }
+    }
+    //*si no hay ganador
+    return null;
+  };
+
+  const updateBoard = (index: number) => {
+    if (board[index] || winner) return;
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="w-fit m-[40px auto] text-center">
+      <h1 className={`text-5xl font-semibold ${openSans.className} mb-[16px]`}>
+        Tic tac toe
+      </h1>
+      <section className="grid grid-cols-3 gap-[10px]">
+        {board.map((_, index) => {
+          return (
+            <Square
+              key={index}
+              index={index}
+              isSelected={false}
+              updateBoard={updateBoard}
+              hasBorder={true}
+            >
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="grid grid-cols-2 place-items-center gap-[10px] mt-5">
+        <Square isSelected={turn === TURNS.X} hasBorder={false}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O} hasBorder={false}>
+          {TURNS.O}
+        </Square>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {winner !== null && (
+        <section className="grid absolute w-screen h-screen top-0 left-0 place-items-center bg-[rgba(0,0,0,0.7)]">
+          <div className="bg[#111] h-[300px] w-[320px] border border-white rounded-md flex flex-col justify-center items-center gap-5 bg-[#111]">
+            <h2>{winner === false ? "Empate" : "Ganó:"}</h2>
+            <header className="m-[0 auto] w-fit border border-white rounded-lg flex gap-4">
+              {winner && <Square>{winner}</Square>}
+            </header>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <footer>
+              <button className="py-[8px] px-[12px] m-[25px] w-[200px] bg-transparent border border-white rounded-md duration-[0.2s] font-bold hover:bg-white hover:text-black">
+                Empezar de nuevo
+              </button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
